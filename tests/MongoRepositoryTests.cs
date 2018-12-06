16057c58a4bbc8cd3328cf1e3b5da1de9965e6a4
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2018-12-05 19:05:13 -0500

    Unit test improvements

--- tests/MongoRepositoryTests.cs
+++ tests/MongoRepositoryTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
 using MongoDB.Driver;
@@ -58,28 +59,59 @@ namespace Foundation.ObjectService.WebUI.Tests
         }
 
         [Fact]
-        public void Insert_Success()
+        public async Task Insert_Object_Success()
         {
             MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
             string json = "{ \"Name\" : \"John\" }";
 
-            var insertResult = repo.InsertAsync("bookstore", "users", 1, json).Result;
-            var getResult = repo.GetAsync("bookstore", "users", 1).Result;
+            var insertResult = await repo.InsertAsync("bookstore", "users", 1, json);
+            var getResult = await repo.GetAsync("bookstore", "users", 1);
 
             Assert.Equal("{ \"_id\" : 1, \"Name\" : \"John\" }", insertResult);
             Assert.Equal("{ \"_id\" : 1, \"Name\" : \"John\" }", getResult);
         }
 
+        // Test takes too long, disabling
+        
+        // [Fact]
+        // public async Task Insert_Object_Fail_no_Database()
+        // {
+        //     MongoClientSettings settings = new MongoClientSettings();
+        //     settings.ConnectTimeout = new TimeSpan(0, 0, 2);
+        //     settings.Server = new MongoServerAddress("localhost", 5555); // wrong port, no DB there
+
+        //     var mongoClient = new MongoClient(settings);             
+        //     MongoRepository repo = new MongoRepository(mongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
+            
+        //     string json = "{ \"Name\" : \"John\" }";
+
+        //     try 
+        //     {
+        //         var insertResult = await repo.InsertAsync("bookstore", "users", 1, json);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Assert.IsType<System.TimeoutException>(ex);
+        //     }
+        // }
+
         [Fact]
-        public void Insert_Does_Not_Overwrite()
+        public async Task Insert_Object_Does_Not_Overwrite()
         {
             MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
             string json = "{ \"Name\" : \"Jane\" }";
 
-            var insertResult = repo.InsertAsync("bookstore", "users", 2, json).Result;
-            Assert.Throws<System.AggregateException>(() => 
-                repo.InsertAsync("bookstore", "users", 2, "{ \"Name\": \"John\" }").Result
-            );
+            var insertResult = await repo.InsertAsync("bookstore", "users", 2, json);
+            
+            try 
+            {
+                var secondInsertResult = await repo.InsertAsync("bookstore", "users", 2, "{ \"Name\": \"John\" }");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsType<MongoDB.Driver.MongoWriteException>(ex);
+            }
+
             var getResult = repo.GetAsync("bookstore", "users", 2).Result;
 
             Assert.Equal("{ \"_id\" : 2, \"Name\" : \"Jane\" }", insertResult);
@@ -87,32 +119,32 @@ namespace Foundation.ObjectService.WebUI.Tests
         }
 
         [Fact]
-        public void Delete_Success()
+        public async Task Delete_Object_Success()
         {
             MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
             string json = "{ \"Name\" : \"Maria\" }";
 
-            var insertResult = repo.InsertAsync("bookstore", "users", 3, json).Result;
-            var getResult = repo.GetAsync("bookstore", "users", 3).Result;
-            var deleteResult = repo.DeleteAsync("bookstore", "users", 3).Result;
-            var getResultAfterDelete = repo.GetAsync("bookstore", "users", 3).Result;
+            var insertResult = await repo.InsertAsync("bookstore", "users", 3, json);
+            var getResult = await repo.GetAsync("bookstore", "users", 3);
+            var deleteResult = await repo.DeleteAsync("bookstore", "users", 3);
+            var getResultAfterDelete = await repo.GetAsync("bookstore", "users", 3);
 
             Assert.Null(getResultAfterDelete);
             Assert.True(deleteResult);
         }
 
         [Fact]
-        public void Replace_Success()
+        public async Task Replace_Object_Success()
         {
             MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
             string json1 = "{ \"Name\" : \"Enrique\" }";
             string json2 = "{ \"Name\" : \"Enrique Hernandez\" }";
 
-            var insertResult = repo.InsertAsync("bookstore", "users", 4, json1).Result;
-            var getResult1 = repo.GetAsync("bookstore", "users", 4).Result;
+            var insertResult = await repo.InsertAsync("bookstore", "users", 4, json1);
+            var getResult1 = await repo.GetAsync("bookstore", "users", 4);
 
-            var replaceResult = repo.ReplaceAsync("bookstore", "users", 4, json2).Result;
-            var getResult2 = repo.GetAsync("bookstore", "users", 4).Result;
+            var replaceResult = await repo.ReplaceAsync("bookstore", "users", 4, json2);
+            var getResult2 = await repo.GetAsync("bookstore", "users", 4);
             
             Assert.Equal("{ \"_id\" : 4, \"Name\" : \"Enrique\" }", getResult1);
             Assert.Equal("{ \"_id\" : 4, \"Name\" : \"Enrique Hernandez\" }", getResult2);
@@ -121,18 +153,40 @@ namespace Foundation.ObjectService.WebUI.Tests
             Assert.Equal(replaceResult, getResult2);
         }
 
+        [Fact]
+        public async Task Replace_Object_Fail_Immutable()
+        {
+            var immutables = new Dictionary<string, HashSet<string>>();
+            immutables.Add("bookstore", new HashSet<string>() { "accounts" });
+
+            MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, immutables);
+            string json = "{ \"Name\" : \"Enrique\" }";
+
+            try 
+            {
+                var insertResult = await repo.InsertAsync("bookstore", "accounts", 1, json);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsType<Foundation.ObjectService.Exceptions.ImmutableCollectionException>(ex);
+            }
+        }
+
         [Theory]
         [InlineData("{ \"Name\": \"John\" " /* missing closing brace */)]
         [InlineData("\"Name\": \"John\" }" /* missing opening brace */)]
         [InlineData("{ \"Name: \"John\" }" /* property is missing end quote */)]
-        public void Insert_Fail_Bad_Json(string badJson)
+        public async Task Insert_Object_Fail_Bad_Json(string badJson)
         {
             MongoRepository repo = new MongoRepository(_mongoFixture.MongoClient, _mongoFixture.Logger, new Dictionary<string, HashSet<string>>());
-
-            Assert.Throws<AggregateException>(() =>
+            try 
             {
-                var result = repo.InsertAsync("bookstore", "users", 1, badJson).Result;
-            });
+                var result = await repo.InsertAsync("bookstore", "users", 1, badJson);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsAssignableFrom<Newtonsoft.Json.JsonException>(ex);
+            }
         }
     }
 
@@ -148,9 +202,7 @@ namespace Foundation.ObjectService.WebUI.Tests
         public MongoFixture()
         {
             Logger = new Mock<ILogger<MongoRepository>>().Object;
-
             _runner = MongoDbRunner.Start();
-
             MongoClient = new MongoClient(MongoConnectionString);
         }
 
